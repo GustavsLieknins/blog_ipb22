0bcf8e648ef2fcba18163f4c4fc1ae1e0c482e5d
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-03-04 15:19:08 +0200

    added

--- functions.php
+++ functions.php
@@ -2,10 +2,8 @@
 
 function dd($datas)
 {
-    echo "<pre>" . "<ul>";
-    foreach ($datas as $data) {
-        echo "<li>" . $data["title"] . "</li>";
-    }
-    echo "</pre>" . "</ul>";
+    echo "<pre>";
+    var_dump($datas);
+    echo "</pre>";
     die();
 };
--- index.php
+++ index.php
@@ -4,14 +4,50 @@ require "functions.php";
 require "Database.php";
 $config = require "config.php";
 
-echo "Hello world";
+echo "<form>";
+echo "<input name='id' />";
+echo "<button>Submit</button>";
+echo "</form>";
 
+// $id = $_GET["id"];
+// dd($id);
+
+
+echo "<h1>Posts</h1>";
+
+$query = "SELECT * FROM posts";
+
+if(isset($_GET["id"]))
+{
+    // $meow = 0;
+    $id = $_GET["id"];
+    if($id == "all"){
+        $query = "SELECT * FROM posts";
+        // $meow = 1; 
+    }else if($id == ""){
+        $query = "SELECT * FROM posts"; 
+        // $meow = 1;
+    }else{
+        $query = "SELECT * FROM posts WHERE id=$id";
+    }
+
+    // if($meow == 0){
+    //     $query = "SELECT * FROM posts";
+    // }
+}
 
 $db = new DataBase($config);
-$posts = $db->execute("SELECT * FROM posts")->fetchALL();
+$posts = $db->execute($query)->fetchALL();
 
 // $cats = $db->execute("SELECT * FROM cats")->fetchALL();
 
 // $days = $db->execute("SELECT * FROM days")->fetchALL();
 
-dd($posts);
+echo "<pre>" . "<ul>";
+foreach ($posts as $data) {
+    echo "<li>" . $data["title"] . "</li>";
+}
+echo "</pre>" . "</ul>";
+
+
+
