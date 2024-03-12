5c7053c0d09ef8f8ef719e7c3168d8bd588708fc
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-03-12 13:30:48 +0200

    added

--- index.php
+++ index.php
@@ -4,36 +4,28 @@ require "functions.php";
 require "Database.php";
 $config = require "config.php";
 
-echo "<form>";
-echo "<input name='id' value='" . ($_GET["id"] ?? '') . "'/>";
-echo "<br>";
-echo "<button>Submit</button>";
-echo "</form>";
-echo "<form>";
-echo "Category: ";
-echo "<select name='category' />";
-echo "<option value='sport'" . ($_GET["category"] == 'sport' ? "selected" : '') . "  >Sport</option>";
-echo "<option value='music'" . ($_GET["category"] == 'music' ? "selected" : '') . " >Music</option>";
-echo "<option value='food'" . ($_GET["category"] == 'food' ? "selected" : '') . "  >Food</option>";
-echo "</select>";
-echo "<br>";
-echo "<button>Submit</button>";
-echo "</form>";
 // $id = $_GET["id"];
 // dd($id);
 
 
-echo "<h1>Posts</h1>";
 
 if(isset($_GET["category"]))
 {
-    $categories = $_GET["category"];
-    $params = [];
-    $query = "SELECT *
-    FROM posts 
-    JOIN categories ON posts.category_id = categories.id
-    WHERE categories.name = :category_name;";
-    $params = [":category_name" => $categories];
+    if($_GET["category"] == "show_all"){
+        $categories = $_GET["category"];
+        $params = [];
+        $query = "SELECT *
+        FROM posts 
+        JOIN categories ON posts.category_id = categories.id";
+    }else{
+        $categories = $_GET["category"];
+        $params = [];
+        $query = "SELECT *
+        FROM posts 
+        JOIN categories ON posts.category_id = categories.id
+        WHERE categories.name = :category_name;";
+        $params = [":category_name" => $categories];
+    }
 };
 
 
@@ -80,18 +72,23 @@ if(isset($_GET["id"]))
 //     $query = "SELECT * FROM posts"; 
 // }
 
-$db = new DataBase($config);
-$posts = $db->execute($query, $params)->fetchALL();
+if(isset($query) || isset($params) ){
+    $db = new DataBase($config);
+    $posts = $db->execute($query, $params)->fetchALL();
+}else{
+    $query = "SELECT * FROM posts"; 
+    $params = [];
+    $db = new DataBase($config);
+    $posts = $db->execute($query, $params)->fetchALL();
+}
+
 
 // $cats = $db->execute("SELECT * FROM cats")->fetchALL();
 
 // $days = $db->execute("SELECT * FROM days")->fetchALL();
 
-echo "<pre>" . "<ul>";
-foreach ($posts as $data) {
-    echo "<li>" . $data["title"] . "</li>";
-}
-echo "</pre>" . "</ul>";
 
 
 
+require "index.view.php";
+?>
---
+++ index.view.php
@@ -0,0 +1,23 @@
+<form>
+    <input name='id' value="<?= ($_GET["id"] ?? '') ?>" />
+    <br>
+    <button>Submit</button>
+    </form>
+    <form>
+    Category: 
+    <select name='category' >
+    <option value='show_all' <?= (isset($_GET["category"]) && $_GET["category"] == 'show_all' ? "selected" : '') ?> >All categories</option>
+    <option value='sport' <?= (isset($_GET["category"]) && $_GET["category"] == 'sport' ? "selected" : '') ?> >Sport</option>
+    <option value='music' <?= (isset($_GET["category"]) && $_GET["category"] == 'music' ? "selected" : '') ?> >Music</option>
+    <option value='food'  <?= (isset($_GET["category"]) && $_GET["category"] == 'food' ? "selected" : '') ?> >Food</option>
+    </select>
+    <br>
+    <button>Submit</button>
+    </form>
+    <hr>
+    <h1>Posts</h1>
+    <ul>
+    <?php foreach ($posts as $data) { ?>
+        <li> <?= $data["title"] ?> </li>
+    <?php } ?>
+    </ul>
