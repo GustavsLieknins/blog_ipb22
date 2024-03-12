46df12a41ba520bceba46d4a92db3cdad1689311
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-03-12 15:20:01 +0200

    added

--- story.php
+++ story.php
@@ -1,7 +1,8 @@
 <?php
+
 $page_title = "Stories";
 $title = "Seksiga gustava blogs";
-$programmers = ["Jānis Mārtiņš", "Oskars", "Emīls", "Edmunds", "Oskars", "Adrians", "Ralfs", "Railijs Didzis", "Kristers", "Roberts Aldis", "Gustavs", "Kristaps Imants", "Elvis", "Adrians", "Matīss Mareks", "Markus", "Kristers", "Marsels", "Kristaps Toms", "Eduards", "Jēkabs"] ;
+$programmers = ["Jānis Mārtiņš", "Oskars", "Emīls", "Edmunds", "Oskars", "Adrians", "Ralfs", "Railijs Didzis", "Kristers", "Roberts Aldis", "Gustavs", "Kristaps Imants", "Elvis", "Adrians", "Matīss Mareks Seksigais", "Markus", "Kristers", "Marsels", "Kristaps Toms", "Eduards", "Jēkabs"] ;
 
 require "views/story.view.php";
 ?>
--- views/components/navbar.php
+++ views/components/navbar.php
@@ -1,3 +1,39 @@
+<style>
+    header 
+    {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+    }
+    nav
+    {
+        display: flex;
+        /* flex-direction: row; */
+        justify-content: space-between;
+        align-items: center;
+        background-color: pink;
+        padding-left: 50px;
+        padding-right: 50px;
+        height: 35px;
+    }
+    a
+    {
+        all: unset;
+        font-family: "Whisper", cursive;
+        font-size: 40px;
+    }
+    body
+    {
+        margin-top: 50px;
+        background-color: #8F95B2;
+    }
+    li 
+    {
+        font-family: "Whisper", cursive;
+    }
+</style>
+
 <header>
     <nav>
         <a href="index.php">Posts</a>
